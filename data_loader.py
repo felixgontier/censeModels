86e0358f449686ce8031db11381ee719d3b57911
mathieu <>
2022-12-12 15:39:19 +0100

    removing low abnd zeroing

--- data_loader.py
+++ data_loader.py
@@ -28,28 +28,28 @@ class PresPredDataset(torch.utils.data.Dataset):
         if 'zero_bands_lower' in settings.keys():
             self.zero_bands_lower = settings['zero_bands_lower']
         self.nClasses = len(self.classes)
-        
+
         if not os.path.isfile(self.datasetPath+'_'+subset+'_tob.npy'):
             assert settings['audio_dir'] is not None, 'No location for dataset audio files specified.'
             if not os.path.exists(settings['root_dir']):
                 os.makedirs(settings['root_dir'])
-            
+
             self.create_dataset(os.path.join(settings['audio_dir'], self.datasetName+'_'+subset), settings['root_dir'], subset=subset, padExamples=settings['pad_examples'])
-        
+
         self.data_tob = np.load(self.datasetPath+'_'+subset+'_tob.npy', mmap_mode='r')
         self.data_pres = np.load(self.datasetPath+'_'+subset+'_pres.npy', mmap_mode='r')
-        
+
         self.nFrames = self.data_pres.shape[1]
         self.allowExampleOverlap = settings['allow_example_overlap']
-        
+
         print('{} dataset {} split length: {}'.format(self.datasetName, subset, self.__len__()))
-        
+
     def create_dataset(self, location, outputDir, subset='train', padExamples=False):
         print('create dataset from audio files at', location)
         files, pres_files = list_all_audio_files(os.path.join(location, 'sound'), os.path.join(location, 'pres_profile'))
-        
+
         tob_transform = ThirdOctaveTransform(self.sr, self.fLen, self.hLen)
-        
+
         # Dummy extraction for dimensions
         x, _ = lr.load(path=files[0], sr=self.sr, mono=True)
         if padExamples:
@@ -58,10 +58,10 @@ class PresPredDataset(torch.utils.data.Dataset):
             nFrames = int(np.floor(((x.shape[0]-(self.tLen*self.fLen))/self.hLen)+1)) # assume equal for all files
         nBands = 29
         nFiles = len(files)
-        
+
         x_tob = np.zeros((nFiles, nFrames+(self.tLen-1), nBands)) # nFrames+(self.tLen-1) = number of 125ms frames
         x_pres = np.zeros((nFiles, nFrames, self.nClasses))
-        
+
         for iF, file in enumerate(files):
             print(' -> Processed ' + str(iF) + ' of ' + str(nFiles) + ' files')
             print(pres_files[iF])
@@ -74,20 +74,20 @@ class PresPredDataset(torch.utils.data.Dataset):
                     for row in csvreader:
                         if len(row) == nFrames:
                             x_pres_temp[iC, :] = np.asarray([[float(l) for l in row]])
-            
+
             x_tob[iF, :, :] = tob_transform.wave_to_third_octave(x, padExamples).T
             x_pres[iF, :, :] = x_pres_temp.T
-            
+
         np.save(os.path.join(outputDir, self.datasetName+'_'+subset+'_tob.npy'), x_tob)
         np.save(os.path.join(outputDir, self.datasetName+'_'+subset+'_pres.npy'), x_pres)
-        
+
     def __getitem__(self, idx):
         if self.seqLen == 1: # CNN
             iFile = int(np.floor(idx/self.nFrames)) # Index of file
             iEx = np.mod(idx, self.nFrames) # Index of exemple within file
             input_x = torch.unsqueeze(torch.from_numpy(self.data_tob[iFile, iEx:iEx+self.tLen, :]), 0)
             pres = torch.unsqueeze(torch.from_numpy(self.data_pres[iFile, iEx, :self.nClasses]), 0)
-            
+
             return F.pad(input_x+self.lvlOffset, (0, 3)), pres # Pad last dimension (freq) from 29 to 32 for the encoder, plus level correction
         else: # RNN
             if self.allowExampleOverlap:
@@ -102,8 +102,8 @@ class PresPredDataset(torch.utils.data.Dataset):
                 pres = torch.unsqueeze(torch.from_numpy(self.data_pres[iFile, iEx*self.seqLen:(iEx+1)*self.seqLen, :self.nClasses]), 0)
             #print(input_x)
             # ----- ZEROS ON BANDS LOWER THAN 100Hz ----- TODO
-            input_x = input_x.type(torch.FloatTensor)
-            input_x[:,:,:self.zero_bands_lower] = -self.lvlOffset
+            # input_x = input_x.type(torch.FloatTensor)
+            # input_x[:,:,:self.zero_bands_lower] = -self.lvlOffset
             # ----- ZEROS ON BANDS LOWER THAN 100Hz ----- TODO
             return F.pad(input_x+self.lvlOffset, (0, 3)), pres # Pad last dimension (freq) from 29 to 32 for the encoder, plus level correction
 
@@ -130,7 +130,7 @@ def wav_to_npy_no_labels(settings, dataPath, datasetName):
         #print(x_tob[-1][5,:]+101)
         print(' -> Processed ' + str(iF+1) + ' of ' + str(len(files)) + ' files')
     np.save(os.path.join(dataPath, datasetName+'_spectralData.npy'), x_tob, allow_pickle=True)
-    
+
 def list_all_audio_files(audio_location, pres_location=None):
     pres_files = []
     audio_files = []
@@ -143,4 +143,3 @@ def list_all_audio_files(audio_location, pres_location=None):
     if len(audio_files) == 0:
         print('found no audio files in ' + audio_location)
     return audio_files, pres_files
-    
